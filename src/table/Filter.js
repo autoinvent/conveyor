1b9ef7e3d0e6d636645fc651c1d12ae6fe048c21
Chris Carson <chriscarson81@gmail.com>
2020-11-09 11:11:48 -0800

    removing the undefined id variables from the keys

--- src/table/Filter.js
+++ src/table/Filter.js
@@ -304,7 +304,7 @@ const FilterOptions = ({
   }
   return (
     <React.Fragment>
-      <FlexibleInput key={`FlexibleInput-${id}-${modelName}-${fieldName}`}
+      <FlexibleInput key={`FlexibleInput-${modelName}-${fieldName}`}
         type={inputTypes.SELECT_TYPE}
         onChange={val => onFilterDropdown({
           modelName,
