18efb463aceb08f6d505f8571b589ed7892da0a9
Aidan Glenister <aidan.glenister@moesol.com>
2024-01-16 09:43:34 -0800

    dependencies/format changes for moment

--- src/components/commons/DateTime.tsx
+++ src/components/commons/DateTime.tsx
@@ -1,15 +1,14 @@
-import { BaseProps } from "../../types";
-import moment from 'moment'
+import { BaseProps } from '../../types';
+import moment from 'moment';
 
 interface DateTimeProps extends BaseProps {
-    date?: Date
+  date?: Date;
 }
 
-const DateTime = ({
-    date,
-    }: DateTimeProps) => {
-    // Process the date as needed
-    return moment(date).format('MM / DD / YYYY hh:mm:ss A');
+const DateTime = ({ date }: DateTimeProps) => {
+  console.log(moment.version);
+  // Process the date as needed
+  return moment(date).format('MM / DD / YYYY hh:mm:ss A');
 };
 
-export default DateTime
+export default DateTime;
