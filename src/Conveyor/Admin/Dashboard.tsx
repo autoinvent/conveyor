0f00a81f8059c15857cdb5da9983d7757d262024
Aidan Glenister <aidan.glenister@moesol.com>
2024-04-22 15:32:28 -0700

    adding logos to side bar and titles on hover of side bar

--- src/Conveyor/Admin/Dashboard.tsx
+++ src/Conveyor/Admin/Dashboard.tsx
@@ -4,26 +4,78 @@ import { HTMLAttributes } from 'react';
 import { Link, Outlet } from '@tanstack/react-router';
 
 import Logo from '@/assets/logo.svg?react';
+import { LucideHome, LucideEclipse, LucideBox } from 'lucide-react';
+import React from 'react';
 
 export interface DashboardProps extends HTMLAttributes<HTMLDivElement> {
     className?: string;
 }
 
 export const Dashboard = ({ className, ...props }: DashboardProps) => {
+    const [isHovered, setIsHovered] = React.useState(false); // State to track hover
+    const asideWidth = isHovered ? 'auto' : '14rem';
+    // Function to handle mouse enter event
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    // Function to handle mouse leave event
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
     return (
         <div
             className={className ?? 'flex min-h-screen w-full flex-col bg-[--bg-color] text-[--text-color]'}
             {...props}
         >
-            <aside className='fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-[--fg-accent] sm:flex'>
-                <nav className='flex flex-col items-center gap-4 px-2 sm:py-5'>
+            <aside className={`fixed inset-y-0 left-0 z-10 hidden flex-col border-r bg-[--fg-accent] sm:flex width-${asideWidth}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <nav className={`flex flex-col items-center gap-4 px-2 sm:py-5 width-${asideWidth}`}>
+                    <Link
+                        className={
+                            'group flex h-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base'
+                        }
+                        to='/'
+                    >
+                        <span className='h-9 flex items-center'>
+                            <Logo className='h-9 w-9'/>
+                            {isHovered && <span className="ml-2">{" Conveyor"}</span>}
+                        </span>
+                    </Link>
+                    <Link
+                        className={
+                            'group flex h-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base'
+                        }
+                        to='/'
+                    >
+
+                        <span className="h-9 flex items-center">
+                            <LucideHome />
+                            {isHovered && <span className="ml-2">{" Home"}</span>}
+                        </span>
+                    </Link>
                     <Link
                         className={
                             'group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base'
                         }
                         to='/'
                     >
-                        <Logo />
+                        
+                        <span className="h-9 flex items-center">
+                            <LucideBox />
+                            {isHovered && <span className="ml-2">{" Models"}</span>}
+                        </span>
+                    </Link>
+                    <Link
+                        className={
+                            'group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base'
+                        }
+                        to='/'
+                    >
+                        
+                        <span className="h-9 flex items-center">
+                            <LucideEclipse />
+                            {isHovered && <span className="ml-2">{" Themes"}</span>}
+                        </span>
                     </Link>
                 </nav>
             </aside>
