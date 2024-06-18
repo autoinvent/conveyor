f03750264b26b2e7b2b2d981524c433ec56503e8
robxbob <robx9021@gmail.com>
2024-06-17 18:25:20 -0700

    tested on example projects

--- examples/vite-react-ts/App.tsx
+++ examples/vite-react-ts/App.tsx
@@ -1,8 +1,9 @@
 import { Helmet } from 'react-helmet';
 import ConveyorLogo from '@autoinvent/conveyor/logo.svg';
-import { Conveyor, ModelIndex, ScalarTypes } from '@autoinvent/conveyor';
+import { Conveyor, ModelIndex, ScalarTypes, useTableView } from '@autoinvent/conveyor';
 
 const App = () => {
+  const tableViewOptions = useTableView();
   return (
     <>
       <Helmet>
@@ -16,53 +17,71 @@ const App = () => {
       <div className="flex flex-col">
         <img src={ConveyorLogo} alt="" className="h-8 w-8" />
         <Conveyor
-          valueOptions={{
-            [ScalarTypes.STRING]: (value) => (
-              <b className="text-green-700">{value}</b>
-            ),
+          typeOptions={{
+            [ScalarTypes.STRING]: {
+              valueRenderFn: ({value}) => (
+                <b className="text-green-700">{value}</b>
+              ),
+            } 
           }}
         >
           <ModelIndex
-            model="DisneyCats"
-            fields={[
-              'id',
-              'type',
-              {
-                name: 'name',
-                type: ScalarTypes.STRING,
-                editable: true,
-                required: true,
-              },
-              { name: 'isHappy', type: ScalarTypes.BOOLEAN, editable: true },
-              { name: 'released', type: ScalarTypes.DATETIME, editable: true },
-              { name: 'bestBearFriend', type: 'DisneyBear', editable: true },
-            ]}
-            data={[
-              {
-                id: '1',
-                type: 'Tiger',
-                name: 'Tigger',
-                isHappy: false,
-                released: '1928-10-1',
-                bestBearFriend: { id: '1' },
-              },
-              {
-                id: '2',
-                type: 'Cat',
-                name: 'Duchess',
-                isHappy: true,
-                released: '1994-06-24',
-                bestBearFriend: null,
-              },
-              {
-                id: '3',
-                type: 'Lion',
-                name: 'Simba',
-                isHappy: true,
-                released: '1970-12-24',
-                bestBearFriend: null,
-              },
-            ]}
+                title='Disney Cats'
+                fields={ [
+                  'id',
+                  'type',
+                  {
+                    name: 'name',
+                    type: ScalarTypes.STRING,
+                    editable: true,
+                    rules: { required: 'Name is required.' },
+                  },
+                  {
+                    name: 'isHappy',
+                    type: ScalarTypes.BOOLEAN,
+                    editable: true,
+                  },
+                  { name: 'released', type: ScalarTypes.DATETIME, editable: true },
+                  {
+                    name: 'bestBearFriend',
+                    type: 'DisneyBear',
+                    editable: true,
+                    sortable: false,
+                  },
+                ]}
+                data={[
+                  {
+                    id: '1',
+                    type: 'Tiger',
+                    name: 'Tigger',
+                    isHappy: false,
+                    released: null,
+                    bestBearFriend: { id: '1' },
+                  },
+                  {
+                    id: '2',
+                    type: 'Cat',
+                    name: 'Duchess',
+                    isHappy: true,
+                    released: '1994-06-24T01:56:34.926365',
+                    bestBearFriend: null,
+                  },
+                  {
+                    id: '3',
+                    type: 'Lion',
+                    name: 'Simba',
+                    isHappy: true,
+                    released: '1970-12-24T01:56:34.926365',
+                    bestBearFriend: null,
+                  }
+                ]}
+                onCreate={() => new Promise((resolve) => setTimeout(resolve, 3000))}
+                onUpdate={() => new Promise((resolve) => setTimeout(resolve, 3000))}
+                onDelete={() => new Promise((resolve) => setTimeout(resolve, 3000))}
+                paginationOptions={{
+                  totalDataLength: 514,
+                }}
+                {...tableViewOptions}
           />
         </Conveyor>
       </div>
