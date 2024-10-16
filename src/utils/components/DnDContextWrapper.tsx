6edc73ebd1290eb8cd571e73894e025e775cc8d2
Connor Jennings <connor.jennings@moesol.com>
2024-10-16 14:53:14 -0700

    Brian's Implementation

--- src/utils/components/DnDContextWrapper.tsx
+++ src/utils/components/DnDContextWrapper.tsx
@@ -1,10 +1,13 @@
 import {
+  closestCenter,
+  type Collision,
+  CollisionDescriptor,
+  type CollisionDetection,
   DndContext,
   type DragEndEvent,
-  KeyboardSensor,
   MouseSensor,
+  rectIntersection,
   TouchSensor,
-  closestCenter,
   useSensor,
   useSensors,
 } from '@dnd-kit/core';
@@ -14,6 +17,49 @@ import {
 } from '@dnd-kit/modifiers';
 import { arrayMove } from '@dnd-kit/sortable';
 import type { ReactNode } from 'react';
+
+const customCollisionAlgorithm: CollisionDetection = ({
+  collisionRect,
+  droppableRects,
+  droppableContainers,
+  active,
+  ...rest
+}) => {
+  const draggableWidth = active.rect.current.translated?.width ?? 0;
+  const x = rectIntersection({
+    collisionRect,
+    droppableRects,
+    droppableContainers,
+    active,
+    ...rest,
+  });
+
+  const y = x.sort((a, b) => {
+    // compute percentage of `a` and `b`, which is the proportion of the droppable component that is covered by the draggable component.
+    console.log(a);
+    console.log(b);
+
+    const shadowCastedOnA = a.data?.value * draggableWidth;
+    const shadowCastedOnB = b.data?.value * draggableWidth;
+
+    const percentA = Math.min(
+      shadowCastedOnA / a.data?.droppableContainer.rect.current.width,
+      1,
+    );
+    const percentB = Math.min(
+      shadowCastedOnB / b.data?.droppableContainer.rect.current.width,
+      1,
+    );
+
+    if (percentB === percentA) {
+      return -1;
+    }
+
+    return percentB - percentA;
+  });
+
+  return y;
+};
 
 export interface DnDContextWrapperProps {
   draggable: boolean;
@@ -38,19 +84,18 @@ export const DnDContextWrapper = ({
         distance: 10,
       },
     }),
-    useSensor(KeyboardSensor, {}),
   );
-  const handleDragEnd = ({ active, over }: DragEndEvent) => {
+  const handleDragEnd = ({ active, over, collisions, delta }: DragEndEvent) => {
     if (active && over && active.id !== over.id) {
-      const oldIndex = dndList.indexOf(active.id as string);
-      const newIndex = dndList.indexOf(over.id as string);
+      const oldIndex = dndList.indexOf(active.id.toString());
+      const newIndex = dndList.indexOf(over.id.toString());
       onDnDListChange?.(arrayMove(dndList, oldIndex, newIndex));
     }
   };
 
   return draggable ? (
     <DndContext
-      collisionDetection={closestCenter}
+      collisionDetection={customCollisionAlgorithm}
       modifiers={[restrictToHorizontalAxis, restrictToParentElement]}
       onDragEnd={handleDragEnd}
       sensors={sensors}
