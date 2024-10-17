91a1ac19531a044e81e241fe2e86f1f1cff066dc
NickLitvin <nick.litvinov9@gmail.com>
2024-10-17 11:07:11 -0700

    fixed theme problem

--- src/BasicInputs/DatetimeInput.tsx
+++ src/BasicInputs/DatetimeInput.tsx
@@ -1,27 +1,123 @@
+"use client";
+ 
+import { useState, useEffect, useImperativeHandle, forwardRef, useRef } from "react"
+import { add, format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+ 
+import { cn } from "@/lib/utils";
+import { Button } from "../lib/components/ui/button";
+import { Calendar } from "../lib/components/ui/calendar";
 import {
-  type ComponentPropsWithoutRef,
-  type ElementRef,
-  forwardRef,
-} from 'react';
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "../lib/components/ui/popover";
+import { type Granularity, TimePicker12h, TimePicker24h } from "../lib/components/ui/time-picker";
 
-import type { FormControlChildProps } from '@/Form';
-import { Input } from '@/lib/components/ui/input';
-import { cn } from '@/lib/utils';
+type DatetimeInputRef = {
+  value?: string;
+} & Omit<HTMLButtonElement, 'value'>;
 
-export const DatetimeInput = forwardRef<
-  ElementRef<typeof Input>,
-  FormControlChildProps & ComponentPropsWithoutRef<typeof Input>
->(({ value = '', className, ...inputProps }, ref) => {
-  if (typeof value !== 'string') {
-    throw new Error('value must be of type string.');
+export interface DatetimeInputProps {
+  value?: string;
+  onChange?: (date: string | undefined) => void;
+  granularity?: Granularity;
+  hourCycle?: 12 | 24
+}
+
+export const DatetimeInput = forwardRef<Partial<DatetimeInputRef>, DatetimeInputProps>(
+  (
+    {
+      value,
+      onChange,
+      granularity = "Second",
+      hourCycle = 12,
+      ...props
+    },
+    ref
+  ) => {
+    const [date, setDate] = useState<Date|undefined>( 
+      value && !Number.isNaN(new Date(value)) 
+      ? new Date(value)
+      : undefined
+    );
+    useEffect( () => { 
+      onChange?.(date ? String(date) : undefined);
+    }, [date, onChange])
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
+    useImperativeHandle(
+      ref,
+      () => ({
+        ...buttonRef.current,
+        value,
+      }),
+      [value],
+    );
+   
+    /**
+     * carry over the current time when a user clicks a new day
+     * instead of resetting to 00:00
+     */
+    const handleSelect = (newDay: Date | undefined) => {
+      if (!newDay || !date) return setDate(newDay); 
+
+      const diff = newDay.getTime() - date.getTime();
+      const diffInDays = diff / (1000 * 60 * 60 * 24);
+      const newDateFull = add(date, { days: Math.ceil(diffInDays) });
+      setDate(newDateFull);
+    };
+
+    const getFormatStyle = () => {
+      switch (granularity) {
+        case "Day": return "PPP";
+        case "Hour": return hourCycle === 12 ? "PPP ha" : "PPP HH:00";
+        case "Minute": return hourCycle === 12 ? "PPP h:mm a": "PPP HH:mm";
+        case "Second": return hourCycle === 12 ? "PPP h:mm:ss a": "PPP HH:mm:ss";
+      }
+    }
+   
+    return (
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant={"outline"}
+            className={cn(
+              !date && "text-muted-foreground",
+              "w-max justify-start text-left font-normal dark:[color-scheme:dark]",
+            )}
+            ref={buttonRef}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date ? format(date, getFormatStyle()) : <span>Pick a date</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={(d) => handleSelect(d)}
+            initialFocus
+            {...props}
+          />
+          <div className='border-border border-t p-3'>
+            {
+              hourCycle === 12 ?
+              <TimePicker12h setDate={setDate} date={date} granularity={granularity}/> :
+              <TimePicker24h setDate={setDate} date={date} granularity={granularity}/>
+            }
+            <div className='flex w-full flex-row pt-3'>
+              <Button variant="link" onClick={() => setDate(undefined)} className='p-2'>
+                Clear
+              </Button>
+              <div className="flex-1"/>
+              <Button variant="link" onClick={() => setDate(new Date())} className='p-2'>
+                Today
+              </Button>
+            </div>
+          </div>
+        </PopoverContent>
+      </Popover>
+    );
   }
-  return (
-    <Input
-      type="datetime-local"
-      value={value?.substring(0, 19) ?? ''}
-      className={cn('dark:[color-scheme:dark]', className)}
-      ref={ref}
-      {...inputProps}
-    />
-  );
-});
+)
