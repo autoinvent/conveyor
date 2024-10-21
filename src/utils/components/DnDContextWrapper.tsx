37638a1a8349176844b1b23bdd00e52f07279794
Connor Jennings <connor.jennings@moesol.com>
2024-10-21 10:48:01 -0700

    simple solution

--- src/utils/components/DnDContextWrapper.tsx
+++ src/utils/components/DnDContextWrapper.tsx
@@ -1,65 +1,15 @@
 import {
-  type Collision,
-  CollisionDescriptor,
-  type CollisionDetection,
   DndContext,
   type DragEndEvent,
   MouseSensor,
   TouchSensor,
   closestCenter,
-  rectIntersection,
   useSensor,
   useSensors,
 } from '@dnd-kit/core';
-import {
-  restrictToHorizontalAxis,
-  restrictToParentElement,
-} from '@dnd-kit/modifiers';
+import { restrictToHorizontalAxis } from '@dnd-kit/modifiers';
 import { arrayMove } from '@dnd-kit/sortable';
 import type { ReactNode } from 'react';
-
-function sortCollisionsAsc(_ref: any, _ref2: any) {
-  const {
-    data: { value: a },
-  } = _ref;
-  const {
-    data: { value: b },
-  } = _ref2;
-  return a - b;
-}
-
-function centerOfRectangle(rect: any) {
-  return {
-    x: rect.left + rect.width * 0.5,
-    y: rect.top + rect.height * 0.5,
-  };
-}
-
-function distanceBetween(p1: any, p2: any) {
-  return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
-}
-
-const customCollisionAlgorithm: CollisionDetection = (_ref) => {
-  const { collisionRect, droppableRects, droppableContainers } = _ref;
-  const centerRect = centerOfRectangle(collisionRect);
-  const collisions = [];
-  for (const droppableContainer of droppableContainers) {
-    const { id } = droppableContainer;
-    const rect = droppableRects.get(id);
-    if (rect) {
-      const distBetween = distanceBetween(centerOfRectangle(rect), centerRect);
-      collisions.push({
-        id,
-        data: {
-          droppableContainer,
-          value: distBetween,
-        },
-      });
-    }
-  }
-  console.log(collisions.sort(sortCollisionsAsc))
-  return collisions.sort(sortCollisionsAsc);
-};
 
 export interface DnDContextWrapperProps {
   draggable: boolean;
@@ -95,8 +45,8 @@ export const DnDContextWrapper = ({
 
   return draggable ? (
     <DndContext
-      collisionDetection={customCollisionAlgorithm}
-      modifiers={[restrictToHorizontalAxis, restrictToParentElement]}
+      collisionDetection={closestCenter}
+      modifiers={[restrictToHorizontalAxis]}
       onDragEnd={handleDragEnd}
       sensors={sensors}
     >
