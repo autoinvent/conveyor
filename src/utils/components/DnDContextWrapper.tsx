22f7660c8780e99cc7203ad28c1c229ca1126739
Connor Jennings <connor.jennings@moesol.com>
2024-10-23 17:13:04 -0700

    working solution

--- src/utils/components/DnDContextWrapper.tsx
+++ src/utils/components/DnDContextWrapper.tsx
@@ -2,16 +2,74 @@ import type { ReactNode } from 'react';
 
 import {
   DndContext,
-  type DragEndEvent,
   KeyboardSensor,
   MouseSensor,
   TouchSensor,
-  closestCenter,
+  rectIntersection,
   useSensor,
   useSensors,
 } from '@dnd-kit/core';
-import { restrictToHorizontalAxis } from '@dnd-kit/modifiers';
+import type {
+  CollisionDescriptor,
+  CollisionDetection,
+  DragOverEvent,
+} from '@dnd-kit/core';
+import {
+  restrictToHorizontalAxis,
+  restrictToParentElement,
+} from '@dnd-kit/modifiers';
 import { arrayMove } from '@dnd-kit/sortable';
+
+/**
+ * Sort collisions from greatest to smallest value
+ */
+export function sortCollisionsDesc(
+  { data: { value: a } }: CollisionDescriptor,
+  { data: { value: b } }: CollisionDescriptor,
+) {
+  return b - a;
+}
+
+const customCollisionAlgorithm: CollisionDetection = ({
+  collisionRect,
+  droppableRects,
+  droppableContainers,
+  active,
+  ...rest
+}) => {
+  const draggableWidth = active.rect.current.translated?.width ?? 0;
+
+  const collisions = rectIntersection({
+    collisionRect,
+    droppableRects,
+    droppableContainers,
+    active,
+    ...rest,
+  });
+
+  const sortedCollisions = collisions.sort((a, b) => {
+    // compute percentage of `a` and `b`, which is the proportion of the droppable component that is covered by the draggable component.
+    const shadowCastedOnA = a.data?.value * draggableWidth;
+    const shadowCastedOnB = b.data?.value * draggableWidth;
+
+    const percentA = Math.min(
+      shadowCastedOnA / a.data?.droppableContainer.rect.current.width,
+      1,
+    );
+    const percentB = Math.min(
+      shadowCastedOnB / b.data?.droppableContainer.rect.current.width,
+      1,
+    );
+
+    if (percentB === percentA) {
+      return -1;
+    }
+
+    return percentB - percentA;
+  });
+
+  return sortedCollisions;
+};
 
 export interface DnDContextWrapperProps {
   draggable: boolean;
@@ -38,7 +96,8 @@ export const DnDContextWrapper = ({
     }),
     useSensor(KeyboardSensor, {}),
   );
-  const handleDragEnd = ({ active, over }: DragEndEvent) => {
+
+  const handleDragOver = ({ active, over }: DragOverEvent) => {
     if (active && over && active.id !== over.id) {
       const oldIndex = dndList.indexOf(active.id.toString());
       const newIndex = dndList.indexOf(over.id.toString());
@@ -48,9 +107,9 @@ export const DnDContextWrapper = ({
 
   return draggable ? (
     <DndContext
-      collisionDetection={closestCenter}
-      modifiers={[restrictToHorizontalAxis]}
-      onDragEnd={handleDragEnd}
+      collisionDetection={customCollisionAlgorithm}
+      modifiers={[restrictToHorizontalAxis, restrictToParentElement]}
+      onDragOver={handleDragOver}
       sensors={sensors}
     >
       {children}
