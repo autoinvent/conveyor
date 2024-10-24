96376d6cae30ae1533fa75f709a9523e457e6717
NickLitvin <nick.litvinov9@gmail.com>
2024-10-24 08:42:57 -0700

    fixed DateTime story

--- src/BasicInputs/stories/DatetimeInput.stories.tsx
+++ src/BasicInputs/stories/DatetimeInput.stories.tsx
@@ -1,16 +1,22 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { DatetimeInput } from '../DatetimeInput';
+import { useState } from 'react';
 
 const meta = {
   title: 'Commons/DateTime',
   tags: ['autodocs'],
-  component: DatetimeInput,
-  args: {
-    value: '2024-07-10T23:12:34',
-    granularity: 'Day',
-    hourCycle: 24,
-  },
+  render: () => {
+    const [date, setDate] = useState<string|undefined>('2024-07-10T23:12:34');
+    return (
+      <DatetimeInput 
+        value={date}
+        onChange={setDate}
+        granularity='Day'
+        hourCycle={24}
+      />
+    )
+  }
 } satisfies Meta<typeof DatetimeInput>;
 export default meta;
 
