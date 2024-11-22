95ba69e7119417dd73cff271bbf53bf3e772111b
NickLitvin <nick.litvinov9@gmail.com>
2024-11-22 12:58:35 -0800

    commented out changes

--- src/Table/TableHeaderRow.tsx
+++ src/Table/TableHeaderRow.tsx
@@ -5,7 +5,7 @@ import { TableRow as STableRow } from '@/lib/components/ui/table';
 
 import { TableHead } from './TableHead';
 import { useTableStore } from './useTableStore';
-import { useModelTableStore } from '@/ModelTable';
+import { ColumnOptions, useModelTableStore } from '@/ModelTable';
 
 export interface TableHeaderRowProps extends ComponentProps<typeof STableRow> {
   prefilled?: boolean;
@@ -22,26 +22,40 @@ export const TableHeaderRow = ({
 
   const onUpdate = useModelTableStore((state) => state.onUpdate);
   const onDelete = useModelTableStore((state) => state.onDelete);
+  const readOnly = useModelTableStore((state) => state.tableOptions?.readOnly);
+  const onWidthChange = useModelTableStore(
+    (state) => state.tableOptions?.onWidthChange,
+  );
 
-  useEffect( () => {
-    setTimeout( () => {
-      if (ref.current && columnOptions) {
-        const rem = Number.parseFloat(getComputedStyle(document.documentElement).fontSize)
-        const columnWidth = Object.values(columnOptions).reduce( 
-          (prev, curr) => prev + (curr?.hidden ? 0 : (curr?.width || 200) + 2*rem), 
-          0
-        )
-        const actionWidth = [onUpdate, onDelete].filter( val => val).length * 3*rem
-        const diff = ref.current.scrollWidth - columnWidth - actionWidth;
+  // useEffect( () => {
+  //   setTimeout( () => {
+  //     if (ref.current && columnOptions) {
+  //       const rem = Number.parseFloat(getComputedStyle(document.documentElement).fontSize)
+  //       const columnWidth = Object.values(columnOptions).reduce( 
+  //         (prev, curr) => prev + (curr?.hidden ? 0 : (curr?.width || 200) + 2*rem), 
+  //         0
+  //       )
+  //       const actionWidth = readOnly ? 0 : [onUpdate, onDelete].filter( val => val).length * 3*rem
+  //       const diff = ref.current.scrollWidth - columnWidth - actionWidth;
 
-        const first = Object.values(columnOptions)[3];
-        if (first && diff > 0) {
-          first.width = ( first?.width || 200 ) + diff 
-        }
-        console.log(columnOptions, first)
-      }
-    }, 0)
-  }, [columnOptions, onDelete, onUpdate])
+  //       let mainColumn : [string, ColumnOptions|undefined];
+  //       for (const column of Object.entries(columnOptions)) {
+  //         console.log(column);
+  //         if ([undefined, true].includes(column[1]?.resizable) === undefined && !column[1]?.hidden) {
+  //           mainColumn = column;
+  //           break;
+  //         }
+  //       }
+  //       // console.log(mainColumn)
+
+  //       // const first = Object.entries(columnOptions)[3];
+  //       // if (first[1] && diff > 0) {
+  //       //   first[1].width = ( first[1]?.width || 200 ) + diff 
+  //       //   onWidthChange?.({ field: first[0], width : first[1]?.width })
+  //       // }
+  //     }
+  //   }, 0)
+  // }, [columnOptions, onDelete, onUpdate, onWidthChange, readOnly])
 
   return (
     <STableRow ref={ref} {...htmlProps}>
@@ -49,7 +63,7 @@ export const TableHeaderRow = ({
         {children === undefined || prefilled ? (
           <>
             {columnIds.map((columnId) => {
-              return <TableHead key={columnId} columnId={columnId} />;
+              return <TableHead key={columnId} columnId={columnId}/>;
             })}
             {children}
           </>
