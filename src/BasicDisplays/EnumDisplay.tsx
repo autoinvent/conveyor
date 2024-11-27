9cc3ef3375bfeb51cb0c53f6b04be5e8bd7c2e50
Robert Hernandez <robert.hernandez@moesol.com>
2024-11-27 02:06:53 -0800

    1.9.1

--- src/BasicDisplays/EnumDisplay.tsx
+++ src/BasicDisplays/EnumDisplay.tsx
@@ -5,15 +5,9 @@ import { humanizeText } from '@/utils';
 import { ModelDisplay, type ModelDisplayProps } from './ModelDisplay';
 
 export const EnumDisplay = forwardRef<HTMLSpanElement, ModelDisplayProps>(
-  (
-    {
-      getDisplayValue = (val) => {
-        return val ? humanizeText(val) : 'None';
-      },
-      ...props
-    },
-    ref,
-  ) => {
-    return <ModelDisplay ref={ref} {...props} />;
+  ({ getDisplayValue = (val) => humanizeText(val), ...props }, ref) => {
+    return (
+      <ModelDisplay ref={ref} getDisplayValue={getDisplayValue} {...props} />
+    );
   },
 );
