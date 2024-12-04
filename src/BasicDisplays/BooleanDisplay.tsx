e0aca39d7288b88eff501fb17c1e0f27be04b498
NickLitvin <nick.litvinov9@gmail.com>
2024-12-04 11:00:03 -0800

    added boolean display fix

--- src/BasicDisplays/BooleanDisplay.tsx
+++ src/BasicDisplays/BooleanDisplay.tsx
@@ -4,7 +4,7 @@ import { Checkbox } from '@/lib/components/ui/checkbox';
 export const BooleanDisplay = ({ value, ...props }: FormDisplayChildProps) => {
   return (
     <div className="flex items-center" {...props}>
-      <Checkbox checked={value} value={value} disabled={true} />
+      <Checkbox checked={value} value={value ?? ''} disabled={true} />
     </div>
   );
 };
