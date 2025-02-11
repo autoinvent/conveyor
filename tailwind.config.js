727482f36d10a8f9becf13494765a18484fbb2f9
Robert Hernandez <therobxbob@gmail.com>
2025-02-11 02:18:34 -0800

    fixed tailwind accordian

--- tailwind.config.js
+++ tailwind.config.js
@@ -66,14 +66,6 @@ module.exports = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
-        'accordion-up': {
-          from: {
-            height: 'var(--radix-accordion-content-height)'
-          },
-          to: {
-            height: '0'
-          }
-        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
