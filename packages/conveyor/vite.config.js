768273441449e7faa9b64f66a6de97c3a8b90132
Robert Hernandez <therobxbob@gmail.com>
2025-10-10 16:46:36 -0700

    base tool structure

---
+++ packages/conveyor/vite.config.js
@@ -0,0 +1,32 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+	plugins: [react()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/index.ts'),
+			name: 'Conveyor',
+			fileName: 'conveyor',
+		},
+		sourcemap: true,
+		rollupOptions: {
+			external: ['react', 'react-dom'],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+				},
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			'~': resolve(__dirname, 'src'),
+		},
+	},
+});
